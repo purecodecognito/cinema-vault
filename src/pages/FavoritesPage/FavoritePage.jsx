0473d6b0185d804b292c119a80c93517532ca6dd
purecodecognito <iamneaytist@gmail.com>
2026-02-15 12:06:42 +0300

    add FavoritePage logic

--- src/pages/FavoritesPage/FavoritePage.jsx
+++ src/pages/FavoritesPage/FavoritePage.jsx
@@ -1,7 +1,34 @@
-import React from 'react'
+import { moviesData } from "../../data/movies";
+import MovieCard from "../../components/MovieCard/MovieCard";
+import styles from '../HomePage/HomePage.module.css';
 
-export default function FavoritePage({isLiked}) {
+export default function FavoritePage({isLiked, handleLike}) {
+
+  const favoriteMovies = moviesData.filter((movie) => 
+    isLiked.includes(movie.id)
+  );
+
+  if (favoriteMovies.length === 0) {
+    return (
+      <div className={styles.empty}>
+        <h2>No favorite movies yet 😔</h2>
+        <p>Go back to Home and add some!</p>
+      </div>
+    );
+  }
+
   return (
-    <div>FavoritePage</div>
+    <div className={styles.grid}>
+      {favoriteMovies.map((movie) => (
+        <MovieCard
+          key={movie.id}
+          movie={movie}
+          // На этой странице мы знаем, что они все избранные, 
+          // но лучше проверять для надежности:
+          isLiked={isLiked.includes(movie.id)} 
+          handleLike={() => handleLike(movie.id)}
+        />
+      ))}
+    </div>
   )
 }
